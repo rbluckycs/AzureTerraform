749ffefea998b586c73e0e1803816989fb29213f
Rizwan Haider <rbluckycsedoc@gmail.com>
2026-09-05 20:38:16 +0530

    resource group and storage is created

--- resourceBlock/main.tf
+++ resourceBlock/main.tf
@@ -1,4 +1,12 @@
-resource "azurerm_resource_group" "Terraform_Azure_GRP" {
-  name     = "Terraform_Azure_Group_Name"
+resource "azurerm_resource_group" "terraforazuregroup1" {
+  name     = "terraforazuregroup1"
   location = "North Europe"
+}
+
+resource "azurerm_storage_account" "storageacc05092026" {
+  name                     = "storageacc05092026"
+  resource_group_name      = azurerm_resource_group.terraforazuregroup1.name
+  location                 = "North Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
