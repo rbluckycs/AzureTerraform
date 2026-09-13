3bfc3ee83204f9a8fe6b2dc39af4353979162d66
srhaider <srhaider@outlook.in>
2026-09-13 12:15:34 +0530

    resource main is commited

--- resourceBlock/main.tf
+++ resourceBlock/main.tf
@@ -10,3 +10,15 @@ resource "azurerm_storage_account" "storageacc05092026" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_container" "terraformstoragecontainer1" {
+  name                  = "terraformstoragecontainer1"
+  storage_account_id    = azurerm_storage_account.storageacc05092026.id
+}
+
+resource "azurerm_storage_blob" "terraformblobfile_log" {
+  name                 = "log.html"
+  storage_container_id = azurerm_storage_container.terraformstoragecontainer1.id
+  type                 = "Block"
+  source               = "log.html"
+}
